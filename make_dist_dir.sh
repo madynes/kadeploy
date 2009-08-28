0f3a40f90ecfedd0da9b4cc5e60c646ff8efc57b
ejeanvoi <ejeanvoi@4ab4880d-aebc-4107-8cb6-a8fee4233f52>
2009-08-28 15:48:35 +0000

    Improved the Debian packaging. Fixed a bug in the Fedora rc script.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/kadeploy3/trunk@41 4ab4880d-aebc-4107-8cb6-a8fee4233f52

--- make_dist_dir.sh
+++ make_dist_dir.sh
@@ -2,14 +2,23 @@
 DIST_DIR=$1
 
 mkdir -p $DIST_DIR
-for f in `find . -name .svn -prune -o -name pkg -prune -o -type f -print`
+
+for d in `find . -name .svn -prune -o -name pkg -prune -o -name $DIST_DIR -prune -o -type d -print`
+do
+    dir=`echo $d|sed "s/^.\///"`
+    if [ "$dir" != "." ]
+    then
+	mkdir -p $DIST_DIR/$dir
+    fi
+done
+
+for f in `find . -name .svn -prune -o -name pkg -prune -o -name $DIST_DIR -o -type f -print`
 do
     dir=`dirname $f|sed "s/^.\///"`
     if [ "$dir" = "." ]
     then
 	cp $f $DIST_DIR
     else
-	mkdir -p $DIST_DIR/$dir
 	cp $f $DIST_DIR/$dir
     fi
 done
