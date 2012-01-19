3f708ab7feb253e37047f541d6c2efa898e3eb6a
Luc Sarzyniec <luc.sarzyniec@gmail.com>
2012-01-19 14:33:09 +0100

    Modified configuration script to be executable from the vlan gateways
    Change-Id: Ie8cb9e915d978afed71ec2a236825e3f8b105878

--- addons/testbed/configurekvm.sh
+++ addons/testbed/configurekvm.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 
-SSH_KEY=.ssh/id_rsa
+SSH_KEY=~/.ssh/id_rsa
 TMP_SSH_KEY=/tmp/identity
 
 SCRIPT_BRIDGE=./setupkvmbridge
 SCRIPT_LAUNCH=./launchkvms
 SCRIPT_GENNODES=./genkvmnodefile
+SSH_OPTIONS='-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PreferredAuthentications=publickey'
 
 NETWORK_CIDR=20
 
-
-if [ -n "$1" ]
+if [ $# -lt 2 ]
 then
-  nbkvms=$1
-else
-  echo "usage: $0 <nb_kvm_per_host> [<hostlist>]"
-  echo "When no hostlist specified, using 'kavlan -l' and removing a node from the list to be the kadeploy daemon"
+  echo "usage: $0 <nb_kvm_per_host> <network_address> <hostlist>"
   exit 1
 fi
 
-if [ -n "$2" ]
+nbkvms=$1
+network=$2
+
+if [ -e $3 ]
 then
-  if [ -e $2 ]
-  then
-    hosts=`cat $2 | sort -n`
-  else
-    echo file not found $2
-    exit 1
-  fi
+  hosts=`cat $3 | sort -n`
 else
-  hosts=`kavlan -l | sort -n`
+  echo file not found $3
+  exit 1
 fi
 
 kadaemon=`echo "$hosts" | head -n 1`
-network=`dig +short $kadaemon`
+
+#gwuser=$USER
+#gwhost="kavlan-`kavlan -V`"
 
 hostfile=`tempfile`
 echo "$hosts" | sed -e '1d' > $hostfile
@@ -42,9 +39,20 @@ echo "$hosts" | sed -e '1d' > $hostfile
 echo "Configuring `cat $hostfile | wc -l` nodes" >&2
 echo "" >&2
 
+echo "Configuring ssh agent" >&2
+sagentfile=`tempfile`
+ssh-agent > $sagentfile
+source $sagentfile 1>/dev/null
+ssh-add $SSH_KEY &>/dev/null
+echo "" >&2
+
+#gwhostfile=`ssh -A $SSH_OPTIONS ${gwuser}@$gwhost 'tempfile'`
+#scp $hostfile ${gwuser}@${gwhost}:$gwhostfile
+
 echo 'Copying ssh key and script files' >&2
 stime=`date +%s`
-taktuk -s -n -l root -f $hostfile broadcast put [ $SSH_KEY ] [ $TMP_SSH_KEY ] \; broadcast put [ $SCRIPT_LAUNCH ] [ /tmp ] \; broadcast put [ $SCRIPT_BRIDGE ] [ /tmp ] 1>/dev/null
+taktuk -s -n -l root -c "ssh -A $SSH_OPTIONS" -f $hostfile broadcast put [ $SSH_KEY ] [ $TMP_SSH_KEY ] \; broadcast put [ $SCRIPT_LAUNCH ] [ /tmp ] \; broadcast put [ $SCRIPT_BRIDGE ] [ /tmp ]
+#ssh -A $SSH_OPTIONS ${gwuser}@$gwhost "taktuk -s -n -l root -c 'ssh -A $SSH_OPTIONS' -f $gwhostfile broadcast put [ $SSH_KEY ] [ $TMP_SSH_KEY ] \; broadcast put [ $SCRIPT_LAUNCH ] [ /tmp ] \; broadcast put [ $SCRIPT_BRIDGE ] [ /tmp ] 1>/dev/null"
 let stime=`date +%s`-stime
 
 if [ $? -ne 0 ]
@@ -55,9 +63,11 @@ else
   echo "... done in ${stime} seconds" >&2
 fi
 
+
 echo 'Configuring bridges' >&2
 stime=`date +%s`
-taktuk -s -n -l root -f $hostfile broadcast exec [ /tmp/`basename $SCRIPT_BRIDGE` $USER kavlan-`kavlan -V` ] 1>/dev/null && \
+taktuk -s -n -l root -c "ssh -A $SSH_OPTIONS" -f $hostfile broadcast exec [ /tmp/`basename $SCRIPT_BRIDGE` ] 1>/dev/null
+#ssh -A $SSH_OPTIONS ${gwuser}@$gwhost "taktuk -s -n -l root -c 'ssh -A $SSH_OPTIONS' -f $gwhostfile broadcast exec [ /tmp/`basename $SCRIPT_BRIDGE` ] 1>/dev/null"
 let stime=`date +%s`-stime
 
 if [ $? -ne 0 ]
@@ -71,6 +81,8 @@ fi
 echo 'Creating nodefile' >&2
 nodefile=`tempfile`
 $SCRIPT_GENNODES ${network}/$NETWORK_CIDR -f $hostfile -n $nbkvms > $nodefile
+#gwnodefile=`ssh -A $SSH_OPTIONS ${gwuser}@$gwhost 'tempfile'`
+#scp $nodefile ${gwuser}@${gwhost}:$gwnodefile
 
 if [ $? -ne 0 ]
 then
@@ -82,7 +94,8 @@ fi
 
 echo 'Launching KVMS' >&2
 stime=`date +%s`
-taktuk -s -n -l root -f $hostfile broadcast exec [ cat - \| /tmp/`basename $SCRIPT_LAUNCH` ] \; broadcast input file [ $nodefile ] 1>/dev/null && \
+taktuk -s -n -l root -c "ssh -A $SSH_OPTIONS" -f $hostfile broadcast exec [ cat - \| /tmp/`basename $SCRIPT_LAUNCH` ] \; broadcast input file [ $nodefile ] 1>/dev/null
+#ssh -A $SSH_OPTIONS ${gwuser}@$gwhost "taktuk -s -n -l root -c 'ssh -A $SSH_OPTIONS' -f $gwhostfil broadcast exec [ cat - \| /tmp/`basename $SCRIPT_LAUNCH` ] \; broadcast input file [ $nodefile ] 1>/dev/null"
 let stime=`date +%s`-stime
 
 if [ $? -ne 0 ]
@@ -95,10 +108,18 @@ fi
 #taktuk -s -n -l root -f $hostfile broadcast exec [ 'test $(ps aux | grep kvm | grep -v grep | grep SCREEN | wc -l) -eq $nbkvms || cat - \| i'"/tmp/`basename $SCRIPT_LAUNCH`" ] \; broadcast input file [ $nodefile ] 1>/dev/null
 
 cat $nodefile
-rm $nodefile
-
-rm $hostfile
 
 echo "" >&2
+echo "Cleaning temporary files" >&2
+rm $nodefile
+#ssh -A $SSH_OPTIONS ${gwuser}@$gwhost "rm $gwnodefile"
+
+rm $hostfile
+#ssh -A $SSH_OPTIONS ${gwuser}@$gwhost "rm $gwhostfile"
+
+ssh-agent -k 1>/dev/null
+rm $sagentfile
+echo "" >&2
+
 echo "Kadeploy daemon: $kadaemon (dont forget to use -d option with the bootstrap script)" >&2
 
