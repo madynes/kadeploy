42561908e7723dce96e983aa1e9435c530e9616a
Luc Sarzyniec <luc.sarzyniec@gmail.com>
2012-02-02 17:38:14 +0100

    Use physical machine CPU cores number to limit the number of VMs on a machine
    Change-Id: I2c5624b3af2e2b16a046ac5b83de968094137700

--- addons/testbed/configurekvm.sh
+++ addons/testbed/configurekvm.sh
@@ -21,7 +21,7 @@ TAKTUK_OPTIONS="-s -n -l root -o status -R connector=2 -R error=2"
 
 if [ $# -lt 1 ]
 then
-  echo "usage: $0 <hostlist> [<www_server_nb>]" 
+  echo "usage: $0 <hostlist> [<www_server_nb>]"
   exit 1
 fi
 
@@ -29,7 +29,7 @@ if [ -e $1 ]
 then
   hosts="`cat $1`"
 else
-  echo file not found $1 
+  echo file not found $1
   exit 1
 fi
 
@@ -54,7 +54,7 @@ wwwservers=`echo "$hosts" | head -n $nbservers | sed -e '1,3d'`
 network=`dig +short $kadaemon`
 if [ $? -ne 0 ]
 then
-  echo 'Failed to get network' 
+  echo 'Failed to get network'
   exit 1
 fi
 
@@ -67,51 +67,51 @@ echo "$wwwservers" > $wwwfile
 nbhosts=`cat $hostfile | wc -l`
 if [ $nbhosts -le 0 ]
 then
-  echo 'No nodes left to host VMs' 
+  echo 'No nodes left to host VMs'
   exit 1
 fi
 
 
-echo "Configuring `cat $hostfile | wc -l` nodes" 
-echo "" 
+echo "Configuring `cat $hostfile | wc -l` nodes"
+echo ""
 
-echo "Configuring ssh agent" 
+echo "Configuring ssh agent"
 sagentfile=`tempfile`
 ssh-agent > $sagentfile
 source $sagentfile 1>/dev/null
 ssh-add $SSH_KEY &>/dev/null
-echo "" 
+echo ""
 
 
-echo 'Copying ssh key and script files' 
+echo 'Copying ssh key and script files'
 stime=`date +%s`
 taktuk $TAKTUK_OPTIONS -o default="$TAKTUK_OUTPUT" -c "$SSH_CONNECTOR" -f $hostfile broadcast put [ $SSH_KEY ] [ $TMP_SSH_KEY ] \; broadcast put [ $SCRIPT_LAUNCH ] [ /tmp ] \; broadcast put [ $SCRIPT_BRIDGE ] [ /tmp ] \; broadcast put [ $SCRIPT_CHECK ] [ /tmp ] 2>&1 | grep -v Warning
 
 let stime=`date +%s`-stime
-echo "... done in ${stime} seconds" 
+echo "... done in ${stime} seconds"
 
 
-echo 'Gathering nodes information' 
+echo 'Gathering nodes information'
 hostyamlfile=${TMP_DIR}/$HOSTS_CONF
 echo '---' > $hostyamlfile
 stime=`date +%s`
 taktuk $TAKTUK_OPTIONS -o default="$TAKTUK_OUTPUT" -c "$SSH_CONNECTOR" -f $hostfile broadcast exec [ /tmp/`basename $SCRIPT_CHECK` ] 2>&1 | grep -v Warning >> $hostyamlfile
 
 let stime=`date +%s`-stime
-echo "... done in ${stime} seconds" 
+echo "... done in ${stime} seconds"
 
 
-echo 'Configuring bridges' 
+echo 'Configuring bridges'
 stime=`date +%s`
-taktuk $TAKTUK_OPTIONS -o default="$TAKTUK_OUTPUT" -c "$SSH_CONNECTOR" -f $hostfile broadcast exec [ /tmp/`basename $SCRIPT_BRIDGE` ] 2>&1 | grep -v Warning 
+taktuk $TAKTUK_OPTIONS -o default="$TAKTUK_OUTPUT" -c "$SSH_CONNECTOR" -f $hostfile broadcast exec [ /tmp/`basename $SCRIPT_BRIDGE` ] 2>&1 | grep -v Warning
 
 let stime=`date +%s`-stime
-echo "... done in ${stime} seconds" 
+echo "... done in ${stime} seconds"
 
 exclfile=`tempfile`
 echo "$hosts" | sed -n "1,${nbservers}p" > $exclfile
 
-echo 'Creating nodefile' 
+echo 'Creating nodefile'
 nodefile=${TMP_DIR}/nodefile
 $SCRIPT_GENNODES ${network}/$NETWORK_CIDR -f $hostyamlfile -e $exclfile > $nodefile
 
@@ -119,21 +119,21 @@ rm $exclfile
 
 if [ $? -ne 0 ]
 then
-  echo '  Failed!' 
+  echo '  Failed!'
   exit 1
 else
-  echo '... done' 
+  echo '... done'
 fi
 
-echo 'Launching KVMS' 
+echo 'Launching KVMS'
 stime=`date +%s`
-taktuk $TAKTUK_OPTIONS -o default="$TAKTUK_OUTPUT" -c "$SSH_CONNECTOR" -f $hostfile broadcast exec [ cat - \| /tmp/`basename $SCRIPT_LAUNCH` ] \; broadcast input file [ $nodefile ] 2>&1 | grep -v Warning 
+taktuk $TAKTUK_OPTIONS -o default="$TAKTUK_OUTPUT" -c "$SSH_CONNECTOR" -f $hostfile broadcast exec [ cat - \| /tmp/`basename $SCRIPT_LAUNCH` ] \; broadcast input file [ $nodefile ] 2>&1 | grep -v Warning
 
 let stime=`date +%s`-stime
-echo "... done in ${stime} seconds" 
+echo "... done in ${stime} seconds"
 
-echo "" 
-echo "Cleaning temporary files" 
+echo ""
+echo "Cleaning temporary files"
 
 #rm $nodefile
 #rm $hostfile
@@ -141,12 +141,12 @@ echo "Cleaning temporary files"
 
 ssh-agent -k 1>/dev/null
 rm $sagentfile
-echo "" 
+echo ""
 
-echo "Kadeploy node: $kadaemon" 
-echo "DNS node: $dnsdaemon" 
-echo "DHCP node: $dhcpdaemon" 
-echo "www nodes:" 
-echo "$wwwservers" 
+echo "Kadeploy node: $kadaemon"
+echo "DNS node: $dnsdaemon"
+echo "DHCP node: $dhcpdaemon"
+echo "www nodes:"
+echo "$wwwservers"
 
-echo "kabootstrap options: -V -d $kadaemon -a $dnsdaemon -b $dhcpdaemon -w $wwwfile -f $nodefile -F $hostfile" 
+echo "kabootstrap options: -V -d $kadaemon -a $dnsdaemon -b $dhcpdaemon -w $wwwfile -f $nodefile -F $hostfile"
