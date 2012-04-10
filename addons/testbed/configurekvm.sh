madynes/kadeploy

commit 176c00195b436f5467f4b459c11cc1aeb8663efc

Add DNS forward option "-c" to kabootstrap generated options
Change-Id: Ib880a03fdd715264985249c4156dc9e28f1c580d

addons/testbed/configurekvm.sh
@@ -241,9 +241,5 @@ echo "DHCP node: $dhcpdaemon"
 echo "www nodes:"
 echo "$wwwservers"
 
-if [ -n "$newnet" ]
-then
-  echo "kabootstrap options: -V -n $networkyamlfile -g `hostname` -s $serviceyamlfile -f $nodefile -F $hostfile"
+echo "kabootstrap options: -V -n $networkyamlfile -g `hostname` -s $serviceyamlfile -c dns.`hostname | cut -d '.' -f 2-` -f $nodefile -F $hostfile"
 else
-  echo "kabootstrap options: -V -g `hostname` -s $serviceyamlfile -f $nodefile -F $hostfile"
-fi

commit 56d68df88add6a7eba9dbc56e07091a847f43d50

fix typo introduced in 176c00195b436f5467f4b459c11cc1aeb8663efc
Change-Id: I101c5bdeae04e4c47f56fd8167af9385c2d7ea63

addons/testbed/configurekvm.sh
@@ -242,4 +242,3 @@ echo "www nodes:"
 echo "$wwwservers"
 
 echo "kabootstrap options: -V -n $networkyamlfile -g `hostname` -s $serviceyamlfile -c dns.`hostname | cut -d '.' -f 2-` -f $nodefile -F $hostfile"
-else