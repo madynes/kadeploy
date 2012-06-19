ca919d3e4caf83e60e6450542edd6f12d17e9e77
Emmanuel Jeanvoine <emmanuel.jeanvoine@inria.fr>
2012-06-19 16:20:14 +0200

    Update make_dist_dir.sh since we use git now.
    Change-Id: I692402ab03729b3b7e06478e4afb02ea116aa7fd

--- make_dist_dir.sh
+++ make_dist_dir.sh
@@ -3,7 +3,7 @@ DIST_DIR=`basename $1`
 
 mkdir -p $DIST_DIR
 
-for d in `find . -name .svn -prune -o -name pkg -prune -o -name $DIST_DIR -prune -o -type d -print`
+for d in `find . -name .git -prune -o -name pkg -prune -o -name $DIST_DIR -prune -o -type d -print`
 do
     dir=`echo $d|sed "s/^.\///"`
     if [ "$dir" != "." ]
@@ -12,7 +12,7 @@ do
     fi
 done
 
-for f in `find . -name .svn -prune -o -name pkg -prune -o -name $DIST_DIR -o -type f -print`
+for f in `find . -name .git -prune -o -name pkg -prune -o -name $DIST_DIR -o -type f -print`
 do
     dir=`dirname $f|sed "s/^.\///"`
     if [ "$dir" = "." ]
