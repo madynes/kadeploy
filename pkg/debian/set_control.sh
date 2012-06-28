2edd4ca1db606dd6c52896f784c3231dc6b317e3
Emmanuel Jeanvoine <emmanuel.jeanvoine@inria.fr>
2012-06-28 11:39:58 +0200

    Added the capability to build -dev Debian packages
    Change-Id: I1916ea8e135b594b1ddf925f96f0d65e93b0ad78

--- pkg/debian/set_control.sh
+++ pkg/debian/set_control.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 INPUT_FILE=$1
+PKG=$2
+SUFFIX=$3
 
 MAJOR_VERSION=`cat ../../major_version`
 MINOR_VERSION=`cat ../../minor_version`
@@ -8,4 +10,5 @@ MINOR_VERSION=`cat ../../minor_version`
 sed '
 s/MAJOR_VERSION/'"$MAJOR_VERSION"'/
 s/MINOR_VERSION/'"$MINOR_VERSION"'/
+s/'"$PKG"'/'"$PKG""$SUFFIX"'/
 ' <$INPUT_FILE
