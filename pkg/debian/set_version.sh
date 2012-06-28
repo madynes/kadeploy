7c80ca589027e41449ff6113ac158d930e6017f3
Luc Sarzyniec <luc.sarzyniec@inria.fr>
2012-06-28 20:03:40 +0200

    New packaging versioning system
    Change-Id: I5c78e63291a9cd0777ca989228a83706b9dbc6f4

--- pkg/debian/set_version.sh
+++ pkg/debian/set_version.sh
@@ -4,8 +4,15 @@ INPUT_FILE=$1
 
 MAJOR_VERSION=`cat ../../major_version`
 MINOR_VERSION=`cat ../../minor_version`
+RELEASE_VERSION=`cat ../../release_version`
+
+if [ "$RELEASE_VERSION" = "stable" ]
+then
+	RELEASE_VERSION=
+fi
 
 sed '
 s/MAJOR_VERSION/'"$MAJOR_VERSION"'/
 s/MINOR_VERSION/'"$MINOR_VERSION"'/
+s/RELEASE_VERSION/'"$RELEASE_VERSION"'/
 ' <$INPUT_FILE
