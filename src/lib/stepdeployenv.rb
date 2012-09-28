c48383931f86e08f30b3fbed9d08915ac567a9be
Luc Sarzyniec <luc.sarzyniec@inria.fr>
2012-09-28 16:52:33 +0200

    Integration of the automata in Kadeploy
    Change-Id: Ib993e968054a69162771116bc92f1c5d241b3349

--- src/lib/stepdeployenv.rb
+++ src/lib/stepdeployenv.rb
@@ -9,120 +9,102 @@ require 'macrostep'
 
 module MacroSteps
   class SetDeploymentEnv < MacroStep
-    def initialize(max_retries, timeout, cluster, nodes, queue_manager, reboot_window, nodes_check_window, output, logger)
-
-      super(
-        max_retries,
-        timeout,
-        cluster,
-        nodes,
-        queue_manager,
-        reboot_window,
-        nodes_check_window,
-        output,
-        logger,
-        1
-      )
-
+    def load_config()
+      super()
     end
   end
 
   class SetDeploymentEnvUntrusted < SetDeploymentEnv
-    def microsteps(step)
-      ret = true
-      ret = ret && step.switch_pxe("prod_to_deploy_env", "")
-      ret = ret && step.set_vlan("DEFAULT")
-      ret = ret && step.reboot("soft", (@currentretry == 0))
-      ret = ret && step.wait_reboot
-      ret = ret && step.send_key_in_deploy_env(:tree)
-      ret = ret && step.create_partition_table("untrusted_env")
-      ret = ret && step.format_deploy_part
-      ret = ret && step.mount_deploy_part
-      ret = ret && step.format_tmp_part
-      ret = ret && step.format_swap_part
-      return ret
+    def tasks()
+      [
+        [ :switch_pxe, "prod_to_deploy_env", "" ],
+        [ :set_vlan, "DEFAULT" ],
+        [ :reboot, "soft" ],
+        [ :wait_reboot ],
+        [ :send_key_in_deploy_env, :tree ],
+        [ :create_partition_table, "untrusted_env" ],
+        [ :format_deploy_part ],
+        [ :mount_deploy_part ],
+        [ :format_tmp_part ],
+        [ :format_swap_part ],
+      ]
     end
   end
 
   class SetDeploymentEnvKexec < SetDeploymentEnv
-    def microsteps(step)
-      ret = true
-      ret = ret && step.switch_pxe("prod_to_deploy_env", "")
-      ret = ret && step.set_vlan("DEFAULT")
-      ret = ret && step.create_kexec_repository
-      ret = ret && step.send_deployment_kernel(:tree)
-      ret = ret && step.kexec(
-        'linux',
-        @cluster_config.kexec_repository,
-        @cluster_config.deploy_kernel,
-        @cluster_config.deploy_initrd,
-        @cluster_config.deploy_kernel_args
-      )
-      ret = ret && step.wait_reboot("kexec")
-      ret = ret && step.send_key_in_deploy_env(:tree)
-      ret = ret && step.create_partition_table("untrusted_env")
-      ret = ret && step.format_deploy_part
-      ret = ret && step.mount_deploy_part
-      ret = ret && step.format_tmp_part
-      ret = ret && step.format_swap_part
-      return ret
+    def tasks()
+      [
+        [ :switch_pxe, "prod_to_deploy_env", "" ],
+        [ :set_vlan, "DEFAULT" ],
+        [ :create_kexec_repository ],
+        [ :send_deployment_kernel, :tree ],
+        [ :kexec,
+          'linux',
+          @cluster_config.kexec_repository,
+          @cluster_config.deploy_kernel,
+          @cluster_config.deploy_initrd,
+          @cluster_config.deploy_kernel_args
+        ]
+        [ :wait_reboot, "kexec" ],
+        [ :send_key_in_deploy_env, :tree ],
+        [ :create_partition_table, "untrusted_env" ],
+        [ :format_deploy_part ],
+        [ :mount_deploy_part ],
+        [ :format_tmp_part ],
+        [ :format_swap_part ],
+      ]
     end
   end
 
   class SetDeploymentEnvUntrustedCustomPreInstall < SetDeploymentEnv
-    def microsteps(step)
-      ret = true
-      ret = ret && step.switch_pxe("prod_to_deploy_env")
-      ret = ret && step.set_vlan("DEFAULT")
-      ret = ret && step.reboot("soft", (@currentretry == 0))
-      ret = ret && step.wait_reboot
-      ret = ret && step.send_key_in_deploy_env(:tree)
-      ret = ret && step.manage_admin_pre_install(:tree)
-      return ret
+    def tasks()
+      [
+        [ :switch_pxe, "prod_to_deploy_env" ],
+        [ :set_vlan, "DEFAULT" ],
+        [ :reboot, "soft" ],
+        [ :wait_reboot ],
+        [ :send_key_in_deploy_env, :tree ],
+        [ :manage_admin_pre_install, :tree ],
+      ]
     end
   end
 
   class SetDeploymentEnvProd < SetDeploymentEnv
-    def microsteps(step)
-      ret = true
-      ret = ret && step.check_nodes("prod_env_booted")
-      ret = ret && step.create_partition_table("prod_env")
-      ret = ret && step.format_deploy_part
-      ret = ret && step.mount_deploy_part
-      ret = ret && step.format_tmp_part
-      return ret
+    def tasks()
+      [
+        [ :check_nodes, "prod_env_booted" ],
+        [ :create_partition_table, "prod_env" ],
+        [ :format_deploy_part ],
+        [ :mount_deploy_part ],
+        [ :format_tmp_part ],
+      ]
     end
   end
 
   class SetDeploymentEnvNfsroot < SetDeploymentEnv
-    def microsteps(step)
-      ret = true
-      ret = ret && step.switch_pxe("prod_to_nfsroot_env")
-      ret = ret && step.set_vlan("DEFAULT")
-      ret = ret && step.reboot("soft", (@currentretry == 0))
-      ret = ret && step.wait_reboot
-      ret = ret && step.send_key_in_deploy_env(:tree)
-      ret = ret && step.create_partition_table("untrusted_env")
-      ret = ret && step.format_deploy_part
-      ret = ret && step.mount_deploy_part
-      ret = ret && step.format_tmp_part
-      ret = ret && step.format_swap_part
-      return ret
+    def tasks()
+      [
+        [ :switch_pxe, "prod_to_nfsroot_env" ],
+        [ :set_vlan, "DEFAULT" ],
+        [ :reboot, "soft" ],
+        [ :wait_reboot ],
+        [ :send_key_in_deploy_env, :tree ],
+        [ :create_partition_table, "untrusted_env" ],
+        [ :format_deploy_part ],
+        [ :mount_deploy_part ],
+        [ :format_tmp_part ],
+        [ :format_swap_part ],
+      ]
     end
   end
 
   class SetDeploymentEnvDummy < SetDeploymentEnv
-    def run
-      tid = Thread.new {
-        @queue_manager.next_macro_step(get_macro_step_name, @nodes)
-        @queue_manager.decrement_active_threads
-        finalize()
-      }
-      return tid
+    def start()
+      true
     end
 
-    def microsteps(step)
-      return true
+    def tasks()
+      []
     end
   end
 end
--- src/lib/workflow.rb
+++ src/lib/workflow.rb
@@ -6,6 +6,7 @@
 #Kadeploy libs
 require 'debug'
 require 'nodes'
+require 'automata'
 require 'config'
 require 'cache'
 require 'macrostep'
@@ -23,6 +24,83 @@ require 'thread'
 require 'uri'
 require 'tempfile'
 
+class Workflow < TaskManager
+  include Printer
+  attr_reader :nodes_brk, :nodes_ok, :nodes_ko
+
+  def initialize(nodeset,context={})
+    super(nodeset,context)
+    @nodes_brk = Nodes::NodeSet.new
+    @nodes_ok = Nodes::NodeSet.new
+    @nodes_ko = Nodes::NodeSet.new
+  end
+
+  def tasks()
+    raise 'Should be reimplemented'
+  end
+
+  def create_task(idx,subidx,nodes,context)
+    taskval = get_task(idx,subidx)
+    taskconf = @config[taskval[0].to_sym][:config]
+
+    begin
+      klass = Module.const_get(taskval[0].to_s)
+    rescue NameError
+      raise "Invalid kind of Macrostep #{taskval[0]}"
+    end
+
+    klass.new(
+      taskval[0],
+      idx,
+      subidx,
+      nodes,
+      @queue,
+      context,
+      nil
+    ).config(taskconf)
+  end
+
+  def kill
+    super()
+    @nodes_ok.clean()
+    @nodes.linked_copy(@nodes_ko)
+  end
+
+  def break!(task,nodeset)
+    debug(4,"<<< Add #{nodeset.to_s_fold} from #{task.name} to BRK nodeset")
+  end
+
+  def success!(task,nodeset)
+    debug(4,"<<< Add #{nodeset.to_s_fold} from #{task.name} to OK nodeset")
+  end
+
+  def fail!(task,nodeset)
+    debug(4,"<<< Add #{nodeset.to_s_fold} from #{task.name} to KO nodeset")
+  end
+
+  def retry!(task)
+    log(task.nodes, "retry_step#{task.idx+1}",nil, :increment => true)
+  end
+
+  def timeout!(task)
+    debug(1,
+      "Timeout in [#{task.name}] before the end of the step, "\
+      "let's kill the instance"
+    )
+    task.nodes.set_error_msg("Timeout in the #{task.name} step")
+  end
+
+  def split!(ns,ns1,ns2)
+    debug(1,"Nodeset(#{ns.id}) split into :")
+    debug(1,"  Nodeset(#{ns1.id}): #{ns1.to_s_fold}")
+    debug(1,"  Nodeset(#{ns2.id}): #{ns2.to_s_fold}")
+  end
+
+  def kill!()
+    debug(2,"!!! Kill a #{self.class.name} instance")
+  end
+end
+
 module Managers
   class MagicCookie
   end
@@ -31,189 +109,6 @@ module Managers
   end
 
   class MoveException < RuntimeError
-  end
-
-  class QueueManager
-    @queue_deployment_environment = nil
-    @queue_broadcast_environment = nil
-    @queue_boot_new_environment = nil
-    @queue_process_finished_nodes = nil
-    attr_reader :config
-    @nodes_ok = nil
-    @nodes_ko = nil
-    @mutex = nil
-    attr_accessor :nb_active_threads
-
-    # Constructor of QueueManager
-    #
-    # Arguments
-    # * config: instance of Config
-    # * nodes_ok: NodeSet of nodes OK
-    # * nodes_ko: NodeSet of nodes KO
-    # Output
-    # * nothing
-    def initialize(config, nodes_ok, nodes_ko)
-      @config = config
-      @nodes_ok = nodes_ok
-      @nodes_ko = nodes_ko
-      @mutex = Mutex.new
-      @nb_active_threads = 0
-      @queue_deployment_environment = Queue.new
-      @queue_broadcast_environment = Queue.new
-      @queue_boot_new_environment = Queue.new
-      @queue_process_finished_nodes = Queue.new
-      Thread.abort_on_exception = true
-    end
-
-    # Increment the number of active threads
-    #
-    # Arguments
-    # * nothing
-    # Output
-    # * nothing
-    def increment_active_threads
-      @mutex.synchronize {
-        @nb_active_threads += 1
-      }
-    end
-
-    # Decrement the number of active threads
-    #
-    # Arguments
-    # * nothing
-    # Output
-    # * nothing
-    def decrement_active_threads
-      @mutex.synchronize {
-        @nb_active_threads -= 1
-      }
-    end
-
-    # Test if the there is only one active thread
-    #
-    # Arguments
-    # * nothing
-    # Output
-    # * returns true if there is only one active thread
-    def one_last_active_thread?
-      @mutex.synchronize {
-        return (@nb_active_threads == 1)
-      }
-    end
-
-    # Go to the next macro step in the automata
-    #
-    # Arguments
-    # * current: name of the current macro step (SetDeploymentEnv, BroadcastEnv, BootNewEnv)
-    # * nodes: NodeSet that must be involved in the next step
-    # Output
-    # * raises an exception if a wrong step name is given
-    def next_macro_step(current_step, nodes)
-      if (nodes.set.empty?)
-        raise "Empty node set"
-      else
-        increment_active_threads
-        case current_step
-        when nil
-          @queue_deployment_environment.push(nodes)
-        when "SetDeploymentEnv"
-          @queue_broadcast_environment.push(nodes)
-        when "BroadcastEnv"
-          @queue_boot_new_environment.push(nodes)
-        when "BootNewEnv"
-          @queue_process_finished_nodes.push(nodes)
-        else
-          raise "Wrong step name"
-        end
-      end
-    end
-
-    # Replay a step with another instance
-    #
-    # Arguments
-    # * current: name of the current macro step (SetDeploymentEnv, BroadcastEnv, BootNewEnv)
-    # * cluster: name of the cluster whose the nodes belongs
-    # * nodes: NodeSet that must be involved in the replay
-    # Output
-    # * returns true if the step can be replayed with another instance, false if no other instance is available
-    # * raises an exception if a wrong step name is given    
-    def replay_macro_step_with_next_instance(current_step, cluster, nodes)
-      macro_step = @config.cluster_specific[cluster].get_macro_step(current_step)
-      if not macro_step.use_next_instance then
-        return false
-      else
-        case current_step
-        when "SetDeploymentEnv"
-          @queue_deployment_environment.push(nodes)
-        when "BroadcastEnv"
-          @queue_broadcast_environment.push(nodes)
-        when "BootNewEnv"
-          @queue_boot_new_environment.push(nodes)
-        else
-          raise "Wrong step name"
-        end
-        return true
-      end
-    end
-
-    # Add some nodes in a bad NodeSet
-    #
-    # Arguments
-    # * nodes: NodeSet that must be added in the bad node set
-    # Output
-    # * nothing
-    def add_to_bad_nodes_set(nodes)
-      @nodes_ko.add(nodes)
-      if one_last_active_thread? then
-        #We add an empty node_set to the last state queue
-        @queue_process_finished_nodes.push(Nodes::NodeSet.new)
-      end
-    end
-
-    # Get a new task in the given queue
-    #
-    # Arguments
-    # * queue: name of the queue in which a new task must be taken (SetDeploymentEnv, BroadcastEnv, BootNewEnv, ProcessFinishedNodes)
-    # Output
-    # * raises an exception if a wrong queue name is given
-    def get_task(queue)
-      case queue
-      when "SetDeploymentEnv"
-        return @queue_deployment_environment.pop
-      when "BroadcastEnv"
-        return @queue_broadcast_environment.pop
-      when "BootNewEnv"
-        return @queue_boot_new_environment.pop
-      when "ProcessFinishedNodes"
-        return @queue_process_finished_nodes.pop
-      else
-        raise "Wrong queue name"
-      end
-    end
-
-    # Send an exit signal in order to ask the terminaison of the threads (used to avoid deadlock)
-    #
-    # Arguments
-    # * nothing
-    # Output
-    # * nothing
-    def send_exit_signal
-      @queue_deployment_environment.push(MagicCookie.new)
-      @queue_broadcast_environment.push(MagicCookie.new)
-      @queue_boot_new_environment.push(MagicCookie.new) 
-      @queue_process_finished_nodes.push(MagicCookie.new)
-    end
-
-    # Check if there are some pending events 
-    #
-    # Arguments
-    # * nothing
-    # Output
-    # * return true if there is no more pending events
-    def empty?
-      return @queue_deployment_environment.empty? && @queue_broadcast_environment.empty? && 
-        @queue_boot_new_environment.empty? && @queue_process_finished_nodes.empty?
-    end
   end
 
   class WorkflowManager
