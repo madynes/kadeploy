d7bc84254eea26c9c8d9e2d9a9ea8022a1f288e3
Luc Sarzyniec <luc.sarzyniec@inria.fr>
2012-12-11 14:24:51 +0100

    Use a common detach method for every command that need to be detached and clean old and useless scripts from deployment env scripts
    Change-Id: I3ac3b84334af9302041b3c3df926ca33b70792e4

--- addons/deploy_env_generation/debootstrap/scripts/launch_transfert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# build chain as root using a detached program
-# can be run as many times as there are transfers because the chain can be rebuilt
-
-/sbin/start-stop-daemon -S -x /usr/local/bin/build_chain.sh -b -- $1
