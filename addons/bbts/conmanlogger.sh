4e7f89b24a667933c8967140cc7dc1ce74e00f74
Luc Sarzyniec <luc.sarzyniec@inria.fr>
2013-04-18 15:59:26 +0200

    Conmanlogger now logs using script
    Change-Id: Icb993a316fa98a0d98d7bf52a4671e12540b17e3

--- addons/bbts/conmanlogger.sh
+++ addons/bbts/conmanlogger.sh
@@ -7,138 +7,144 @@ CONMAN_SERVER=conman
 
 function get_nodename()
 {
-	echo ${1%%.*}
+  echo ${1%%.*}
 }
 
-function get_screen_prefix()
+function get_session_name()
 {
-	echo `basename $1 .sh`-$2
+  echo `basename $1 .sh`-$2
 }
 
-function get_screen_name()
+function get_session_file()
 {
-	echo ${1}-$2
+  echo ${1}/.SESSION
 }
 
 function get_logdir()
 {
-	echo `basename $1 .sh`.$2
+  echo `basename $1 .sh`.$2
 }
 
 function get_logfile()
 {
-	echo ${1}/${2}.conlog
+  echo ${1}/${2}.conlog
+}
+
+function get_timefile()
+{
+  echo ${1}/${2}.timing
 }
 
 function check_args()
 {
-	if [ $# -lt 3 ]
-	then
-		echo "usage: $0 <start/stop> <nodefile> <run_id> [<logsdir>]"
-		exit 1
-	fi
+  if [ $# -lt 3 ]
+  then
+    echo "usage: $0 <start/stop> <run_id> <nodefile> [<logsdir>]"
+    exit 1
+  fi
 
-	if [ -e $3 ]
-	then
-		nodes=`uniq $3`
-	else
-		echo "file not found '$3'"
-		exit 1
-	fi
+  if [ -e $3 ]
+  then
+    nodes=`uniq $3`
+  else
+    echo "file not found '$3'"
+    exit 1
+  fi
 
-	if [ $# -ge 4 ]
-	then
-		logdir=$4
-	else
-		logdir=$(get_logdir $0 $run_id)
-	fi
+  if [ $# -ge 4 ]
+  then
+    logdir=$4
+  else
+    logdir=$(get_logdir $0 $run_id)
+  fi
 }
 
 function init_logs()
 {
-	echo "Creating logs directory '$1'"
-	if [ -e "$1" ]
-	then
-		if [ -n "$(ls -A $1)" ]
-		then
-			echo "Backup logs directory"
-			mkdir -p ${1}/bak
-			mv $1/* ${1}/bak &>/dev/null
-		fi
-	else
-		mkdir -p $1
-	fi
+  echo "Creating logs directory '$1'"
+  if [ -e "$1" ]
+  then
+    if [ -n "$(ls -A $1)" ]
+    then
+      echo "Backup logs directory"
+      mkdir -p ${1}/bak
+      mv $1/* ${1}/bak &>/dev/null
+    fi
+  else
+    mkdir -p $1
+  fi
 }
 
 function gz_log()
 {
-	local file=$(get_logfile $1 $2)
-	if [ -e $file ]
-	then
-		gzip $file
-	fi
+  local file=$(get_logfile $1 $2)
+  if [ -e $file ]
+  then
+    gzip $file
+  fi
 }
 
-function init_screen()
+function gz_time()
 {
-	echo -e "\t init $1"
-	screen -d -m -S $1
-	#screen -S $screen_name -X logfile ${logdir}/$screen_name
-	#screen -S $screen_name -X logfile flush 1
-	#screen -S $screen_name -X logtstamp on
-	#screen -S $screen_name -X logtstamp after 10
-	#screen -S $screen_name -X deflog on
-	screen -S $1 -X screen $CONMAN_BIN -d $CONMAN_SERVER \
-		-l $(get_logfile $3 $2) $2
+  local file=$(get_timefile $1 $2)
+  if [ -e $file ]
+  then
+    gzip $file
+  fi
 }
 
-function kill_screen()
+function init_monitor()
 {
-		echo -e "\t kill $1"
-		#screen -S $screen_name -X stuff "&."
-		screen -S $1 -X quit
+  echo -e "\t init $1"
+  script -f -c "$CONMAN_BIN -d $CONMAN_SERVER $1" \
+    -t 2>$(get_timefile $2 $1) -a $(get_logfile $2 $1) 1>/dev/null &
+  echo "$1 $!" >> $3
+}
+
+function kill_monitor()
+{
+  ppid=$(sed -n "s/^$1 \([0-9]\+\)$/\1/p" $2)
+  pids=()
+  for pid in `ps --ppid $ppid -o pid=`
+  do
+    pids+=($pid)
+  done
+  echo -e "\t kill $1 ($ppid $pids)"
+  kill $ppid $pids
 }
 
 
 ### Start
 
-check_args $@
 run_id=$2
+check_args $@
+session_name=$(get_session_name $run_id)
+session_file=$(get_session_file $logdir)
 
 case "$1" in
 start)
+  init_logs $logdir
+  echo "Initialize monitors"
+  for n in $nodes
+  do
+    node_name=$(get_nodename $n)
+    init_monitor $node_name $logdir $session_file
+  done
 
-	init_logs $logdir
-	echo "Initialize screens"
-	for n in $nodes
-	do
-		node_name=$(get_nodename $n)
-		screen_prefix=$(get_screen_prefix $0 $n)
-		screen_name=$(get_screen_name $screen_prefix $run_id)
-
-		if screen -list | grep $screen_prefix &>/dev/null
-		then
-			kill_screen $screen_prefix
-		fi
-		init_screen $screen_name $node_name $logdir
-	done
-
-	echo "Start monitoring"
-	;;
+  echo "Start monitoring"
+  ;;
 stop)
-	echo "Kill screens"
-	for n in $nodes
-	do
-		node_name=$(get_nodename $n)
-		screen_prefix=$(get_screen_prefix $0 $n)
-		screen_name=$(get_screen_name $screen_prefix $run_id)
-		kill_screen $screen_prefix
-		gz_log $logdir $node_name
-	done
+  echo "Kill monitors"
+  for n in $nodes
+  do
+    kill_monitor $n $session_file
+    gz_log $logdir $node_name
+    gz_time $logdir $node_name
+  done
 
-	echo "Stop monitoring"
-	;;
+  echo "Stop monitoring"
+  ;;
 *)
-	echo "unknown command '$1'"
-	;;
+  echo "unknown command '$1'"
+  ;;
 esac
