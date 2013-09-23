a4ae3b793b3f8c944192130b5715376cdcfba2f4
Luc Sarzyniec <luc.sarzyniec@inria.fr>
2013-09-23 18:10:27 +0200

    Add missing file steppower
    Change-Id: If4e16a052c3c02a0d05e80200560b924ade37d79

---
+++ src/lib/steppower.rb
@@ -0,0 +1,31 @@
+require 'macrostep'
+
+module Kadeploy
+
+module Macrostep
+  class PowerOn < Power
+    def steps()
+      [
+        [ :power, :on, context[:execution].level ],
+      ]
+    end
+  end
+
+  class PowerOff < Power
+    def steps()
+      [
+        [ :power, :off, context[:execution].level ],
+      ]
+    end
+  end
+
+  class PowerStatus < Power
+    def steps()
+      [
+        [ :power, :status, nil ],
+      ]
+    end
+  end
+end
+
+end
