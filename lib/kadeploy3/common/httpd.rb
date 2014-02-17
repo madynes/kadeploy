6ba7e12335fc91ffe46184fa805ff1a5358b32ab
Luc Sarzyniec <luc.sarzyniec@inria.fr>
2014-02-17 11:49:04 +0100

    [httpd] Bugfix: Do not force encoding to UTF-8, set the Content-Type to the encoding of the returned String
    Change-Id: I6c88b666dba47c5fb41cab52f0081ed09b4ecca1

--- lib/kadeploy3/common/httpd.rb
+++ lib/kadeploy3/common/httpd.rb
@@ -216,10 +216,8 @@ module HTTPd
         type,content = [ TYPES[:json], JSON.pretty_generate(obj) ]
       end
 
-      # Force encoding to UTF-8
-      content.encode!('UTF-8', {:invalid => :replace, :undef => :replace})
       type = type.dup
-      type << '; charset=utf-8'
+      type << "; charset=\"#{content.encoding.to_s.strip}\""
 
       if @encoding and @encoding.include?('gzip')
         sio = StringIO.new('w')
@@ -370,9 +368,7 @@ module HTTPd
       end
       if response['Content-Type'] == 'text/plain'
         res += "\n"
-        # Force encoding to UTF-8
-        response['Content-Type'] << '; charset=utf-8'
-        res.encode!('UTF-8', {:invalid => :replace, :undef => :replace})
+        response['Content-Type'] << "; charset=\"#{res.encoding.to_s.strip}\""
       end
       response['Content-Length'] = res.size
       response.body = res unless get_method(request) == :HEAD
