e569f03453ab945114b9203bc58d91c76b53f074
Luc Sarzyniec <luc.sarzyniec@inria.fr>
2014-03-24 18:43:56 +0100

    [vagrant] Modify reboot handler to boot the VM if it was powered off
    Change-Id: I2a2a6fd12e4adceaa402ec489240ae339ec179a1

--- addons/puppet4vagranttb/vbox_handler.sh
+++ addons/puppet4vagranttb/vbox_handler.sh
@@ -2,6 +2,15 @@
 while true
   do
     node=$(nc -l -p 25300)
+    [ $? -ne 0 ] && exit 1
     vmid=$(vboxmanage list runningvms|grep ${node}_|cut -f2 -d" "|tr -d "{}")
-    vboxmanage controlvm $vmid reset
+    if [ -n "$vmid" ]
+    then
+      echo "--- reset $node"
+      vboxmanage controlvm $vmid reset
+    else # In case the VM is not running
+      echo "--- start $node"
+      vmid=$(vboxmanage list vms|grep ${node}_|cut -f2 -d" "|tr -d "{}")
+      vboxmanage startvm $vmid --type headless
+    fi
   done
